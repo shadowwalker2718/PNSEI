56d2702ffd5540c8138f86b16e730e628fa338c7
shadowwalker2718 <shadowwalker2718@gmail.com>
2018-10-21 17:23:43 -0400

    Clion

---
+++ c++/cruise/clique.h
@@ -0,0 +1,36 @@
+//
+// Created by henry on 10/21/18.
+//
+#include "henry.h"
+
+#ifndef C_LRU_H
+#define C_LRU_H
+
+/*
+ * [] Cruise onsite面 [复制链接] |试试Instant~
+
+ https://www.1point3acres.com/bbs/forum.php?mod=viewthread&tid=293409&ctid=228575
+
+发个小公司 onsite 面经吧攒个人品，申的是无人驾驶相关类岗位，总共被4个人面，一人一小时，第一个人出题是sparse matrix设计set,get,add，第二个题是图里的connected component, 第三题是图像里面找像素比周围rxr区域都大的点（专业名词heatmap argmax）并优化算法，第四题是leetcode range sum 2d 那个。 感觉题都不难，但是要白板写，从没在白板写过。
+
+被技术问题问倒了，多纬度数据如何存储，查找，这个不难kd-tree，然后区间查找如何o(1)这个真不知道，然后问了些神经网络的东西，我做过一些，但从没研究过神经网络里面模型的意义，被问到了自己调的网络的模型意义，半年前搭的网络早忘了具体模型意义，答不上来，然后问了如何在实际中应用，没有实际相关领域工作经验答不上来。。。
+
+第二天发据信了。
+
+ OA 三题，一个半小时，第一题Pascal's Triangle , 第二题大意是一个robot，可以执行G，L，R，三种操作，对应为直行，左转，右转，现在给定一个操作序列， 如GLR, 或是其他的如LR， 给定的操作直行无限次操作，请问哪种操作序列可以保证robot会停留在一个圆里面活动（即不会一直往前走），第三题是hackrank上的clique题，你可以去看看
+
+ * */
+
+
+namespace cruise_2017_09_20{
+  // http://sde.quant365.com/linkedin-2017-interview-questions-part-3.html#sparse-matrix-storage
+  // https://www.hackerrank.com/challenges/clique/problem
+  // https://www.youtube.com/watch?v=t-_ZWbugSb8
+  // https://www.youtube.com/watch?v=eya-cyk-7-4
+  // https://www.youtube.com/watch?v=qZs767KQcvE
+  class clique{
+  public:
+  };
+}
+
+#endif //C_LRU_H
---
+++ c++/cruise/lru.h
@@ -0,0 +1,16 @@
+//
+// Created by henry on 10/21/18.
+//
+
+#ifndef C_LRU_H
+#define C_LRU_H
+
+/*
+ * https://www.1point3acres.com/bbs/forum.php?mod=viewthread&tid=448226&ctid=228575
+地里找人帮忙内推了一下Infra方向, 形式是 Video Interview, 面试官是个白人小姐姐。地里可参考的面经比较少。自我介绍后题目就是LRU，只不过加了个delete function. 打开coderpad要用python写，问可不可以java，同意后就用java写了。
+先写了每个function的思路，确认后写code，最后一个delete functionc没来得及写code。
+然后问了一下如果这个用于多线程会有什么问题。
+最后例行问问题。求大米呀求大米！！！*/
+
+
+#endif //C_LRU_H
