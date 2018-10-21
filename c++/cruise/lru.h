b14967f1e5617d74b10671065ae860cf4ef2a1e3
shadowwalker2718 <shadowwalker2718@gmail.com>
2018-10-21 17:26:00 -0400

    LRU

--- c++/cruise/lru.h
+++ c++/cruise/lru.h
@@ -5,6 +5,7 @@
 #ifndef C_LRU_H
 #define C_LRU_H
 
+#include "henry.h"
 /*
  * https://www.1point3acres.com/bbs/forum.php?mod=viewthread&tid=448226&ctid=228575
 地里找人帮忙内推了一下Infra方向, 形式是 Video Interview, 面试官是个白人小姐姐。地里可参考的面经比较少。自我介绍后题目就是LRU，只不过加了个delete function. 打开coderpad要用python写，问可不可以java，同意后就用java写了。
@@ -13,4 +14,46 @@
 最后例行问问题。求大米呀求大米！！！*/
 
 
+namespace cruise_20181009{
+
+  class LRUCache {
+    list<pair<int,int>> ls;
+    map<int, list<pair<int,int>>::iterator> m;
+    int cnt;
+  public:
+    LRUCache(int capacity) : cnt(capacity){}
+
+    int get(int key) {
+      if(!m.count(key)) return -1;
+      int r=m[key]->second;
+      ls.erase(m[key]);
+      ls.emplace_front(key, r);
+      return r;
+    }
+
+    void put(int key, int value) {
+      if(m.count(key)){
+        ls.erase(m[key]);
+        ls.emplace_front(key, value);
+        m[key]=ls.begin();
+        return;
+      }
+      if(cnt==ls.size()){
+        m.erase(ls.back().first); // the reason why we need list<Key,Value> instead of list<Value>
+        ls.pop_back();
+      }
+      ls.emplace_front(key,value);
+      m[key]=ls.begin();
+    }
+
+    void del(int key){
+      if(m.count(key)){
+        ls.erase(m[key]);
+        m.erase(key);
+      }
+    }
+  };
+
+
+}
 #endif //C_LRU_H
