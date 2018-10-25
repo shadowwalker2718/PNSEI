170230011f74d2454adb8e2cf8e9c3c7f3cb0c98
shadowwalker2718 <shadowwalker2718@gmail.com>
2018-10-25 00:10:46 -0400

    d

--- cpp/dropbox/id_allocator.h
+++ cpp/dropbox/id_allocator.h
@@ -6,7 +6,7 @@
 #define PNSEI_ID_ALLOCATOR_H
 
 /*
- * https://www.bfilipek.com/2017/08/cpp17-details-filesystem.html
+ * https://www.1point3acres.com/bbs/thread-361979-1-1.html
 
  * 第一轮算法，alloc id. HashTable + Queue
  写了一个简单实现，然后分析这个解法哪里不好，计算了一下memory
@@ -15,6 +15,32 @@
  Tree的解法。面试官说suppose给你的max
  capacity永远是2的N次，帮我降低实现难度，让我实现了一下。中间出了点小bug，被面试官发现了，很快修正了。做完以后还有10分钟左右，面试官问有没有一个解法可以做到O
  constant的复杂度，不是O(1)。
- 想了一会儿没想出来，只想到了从HashMap的角度出发，时间不多了就结束了，最后面试官简单说了一下思路，没太搞明白，但是确实是HashMap的方向。*/
+ 想了一会儿没想出来，只想到了从HashMap的角度出发，时间不多了就结束了，最后面试官简单说了一下思路，没太搞明白，但是确实是HashMap的方向。
+
+
+alloc id 我onsite也遇到了， 最后的最优解法 是建立一个2n个bit的 segment tree 然后o（lg n）的复杂度完成所有操作
+ */
+
+/*
+ * 编程题就一道,给一个数N,生成N个id, 用户可以调用allocate()来分配一个id, 用完后调用release()释放id, 这样别的用户可以用. 一开始给了基于bit的解法,但allocate的复杂度是O(N), 要求改进,并提示memory不是问题. 最后的方法是基于hash的.所有的id生成后放到hash table里,然后从里面取
+
+Dropbox 真是万年题不变啊。 当时我用Tree, 刚好是space有要求
+
+嗯，挺开放的题目，可难可易，取决于那一位面试官的考点在哪儿了
+
+http://www.1point3acres.com/bbs/thread-194896-1-1.html
+
+实现两个函数,int allocate()和void release(intid),每调用一次allocate返回的id需要unique,为1到N之间的一个整数.如果release以后,就可以继续被allocate.之前用array+hashmap,达到O(1)时间和O(N)空间.后来被告知空间用得太多,map空间效率低,最后用了bitmap.这题其实和实现文件系统的metadata区域比较类似,不过最后居然是用时间换空间,有点让我诧异.
+
+https://blog.csdn.net/whuwangyi/article/details/43112895
+
+http://massivealgorithms.blogspot.com/2016/08/leetcode-379-design-phone-directory.html
+
+Related OJ:
+https://leetcode.com/problems/design-phone-directory/
+
+
+
+ * */
 
 #endif // PNSEI_ID_ALLOCATOR_H
--- cpp/dropbox/multithread_web_crawler.h
+++ cpp/dropbox/multithread_web_crawler.h
@@ -5,10 +5,20 @@
 #ifndef PNSEI_MULTITHREAD_WEB_CRAWLER_H
 #define PNSEI_MULTITHREAD_WEB_CRAWLER_H
 
-/*
+/* https://www.1point3acres.com/bbs/thread-361979-1-1.html
  *
- * 第四轮多线程，小哥对我现在做的项目比较感兴趣，简单聊了聊。然后开始web crawler，我问是多线程还是单线程。小哥说好问题，让我说说为什么多线程，单线程慢在哪里，多线程怎么提高速度的。说完以后让我先从单线程开始实现，给了isInternal(url)和getLinks(url)两个API，让实现crawl(root)方法，返回这个domain下所有的links。 很快实现，然后开始多线程版本。我问小哥能不能直接用Executor Service，小哥表示犹豫，说用Executor的话太简单了，希望我用condition varialble来实现。我说可以，但是我想把线程管理和crawl本身的代码分开，这样decouple以后容易管理代码，结构会比较清晰，所以我还是想用Executor Service，我可以快速实现一个simple executor，小哥表示同意。然后用condition variable实现了一个Blocking Queue，又用Blocking Queue实现了一个Executor，不过没有实现线程池，每次有task进来就直接开一个线程来执行这个task。实现过程中问小哥要不要考虑termination，小哥说暂时不用考虑。但是其实最终还是要考虑的，不然程序没办法终止，拿不到结果。最后补上了termination condition，同时讨论了一下Java程序终止是main thread结束就终止还是所有thread都退出才终止。 昨晚以后还有10分钟左右，小哥又问了问我正在做的项目，看来真的是挺感兴趣。
+ * 第四轮多线程，小哥对我现在做的项目比较感兴趣，简单聊了聊。然后开始web
+ * crawler，我问是多线程还是单线程。小哥说好问题，让我说说为什么多线程，单线程慢在哪里，多线程怎么提高速度的。说完以后让我先从单线程开始实现，给了isInternal(url)和getLinks(url)两个API，让实现crawl(root)方法，返回这个domain下所有的links。
+ * 很快实现，然后开始多线程版本。我问小哥能不能直接用Executor
+ * Service，小哥表示犹豫，说用Executor的话太简单了，希望我用condition
+ * varialble来实现。我说可以，但是我想把线程管理和crawl本身的代码分开，这样decouple以后容易管理代码，结构会比较清晰，所以我还是想用Executor
+ * Service，我可以快速实现一个simple executor，小哥表示同意。然后用condition
+ * variable实现了一个Blocking Queue，又用Blocking
+ * Queue实现了一个Executor，不过没有实现线程池，每次有task进来就直接开一个线程来执行这个task。实现过程中问小哥要不要考虑termination，小哥说暂时不用考虑。但是其实最终还是要考虑的，不然程序没办法终止，拿不到结果。最后补上了termination
+ * condition，同时讨论了一下Java程序终止是main
+ * thread结束就终止还是所有thread都退出才终止。
+ * 昨晚以后还有10分钟左右，小哥又问了问我正在做的项目，看来真的是挺感兴趣。
  *
  * */
 
-#endif //PNSEI_MULTITHREAD_WEB_CRAWLER_H
+#endif // PNSEI_MULTITHREAD_WEB_CRAWLER_H
