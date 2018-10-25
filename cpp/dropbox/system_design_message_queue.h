2b1c811b6a87a4574eea84288a03594f465fd50d
shadowwalker2718 <shadowwalker2718@gmail.com>
2018-10-24 23:54:40 -0400

    d

---
+++ cpp/dropbox/system_design_message_queue.h
@@ -0,0 +1,13 @@
+//
+// Created by henry on 10/24/18.
+//
+
+#ifndef PNSEI_SYSTEM_DESIGN_MESSAGE_QUEUE_H
+#define PNSEI_SYSTEM_DESIGN_MESSAGE_QUEUE_H
+
+/*
+ * https://www.1point3acres.com/bbs/thread-361979-1-1.html
+ * 第五轮System Design，design一个message queue service，我主要做iOS development和JS development的，没做过也没用过message queue，对一些需求不是很清楚，只能硬着头皮上，先给了一个非常粗糙的design，然后面试官给我各种各样的use case，不断完善。最后总算是差不多design出来了，不知道面试官满不满意。我对System Design的要求不是很清楚，不知道回答到什么程度算是合格的。
+ * */
+
+#endif //PNSEI_SYSTEM_DESIGN_MESSAGE_QUEUE_H
