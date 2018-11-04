a1d2aaccb6432664727e4cd02f6cada5e161a9ee
shadowwalker2718 <shadowwalker2718@gmail.com>
2018-11-04 09:57:49 -0500

    l

--- cpp/leetcode/935_Knight_Dailer.py
+++ cpp/leetcode/935_Knight_Dailer.py
@@ -4,16 +4,17 @@ class Solution(object):
         :type N: int
         :rtype: int
         """
-        MOD=10**9+7
-        d=[[4,6],[6,8],[7,9],[4,8],[0,3,9],[],[0,1,7],[2,6],[1,3],[2,4]]
-        ans=[1]*10
-        for i in range(N-1):
-            nans=[0]*10
+        MOD = 10 ** 9 + 7
+        d = [[4, 6], [6, 8], [7, 9], [4, 8], [0, 3, 9], [], [0, 1, 7], [2, 6], [1, 3], [2, 4]]
+        ans = [1] * 10
+        for i in range(N - 1):
+            nans = [0] * 10
             for j in range(10):
                 for k in d[j]:
-                    nans[j]+=ans[k]
-            ans=[m%MOD for m in nans]
-        return sum(ans)%MOD
+                    nans[j] += ans[k]
+            ans = [m % MOD for m in nans]
+        return sum(ans) % MOD
 
-sln=Solution()
+
+sln = Solution()
 sln.knightDialer(161)
