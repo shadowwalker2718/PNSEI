a0c54637eb3e411a04bce0c1c75e133c97c98139
shadowwalker2718 <shadowwalker2718@gmail.com>
2018-11-04 21:31:48 -0500

    longest path

---
+++ cpp/addepar/boggle.h
@@ -0,0 +1,43 @@
+//
+// Created by root on 11/4/18.
+//
+
+#ifndef PNSEI_BOGGLE_H
+#define PNSEI_BOGGLE_H
+
+/*
+ * Application
+
+I applied online. I interviewed at Addepar in April 2017.
+
+Interview
+
+I was interviewed in two separate phone interviews. Both interviews were similar: describe how to code a simple
+ algorithm that the interviewer proposed. The coding was online. Both lasted about 45 minutes.
+I thought that I answered the coding questions correctly, but later, I was told that the interview process
+ "won't be moving forward."
+It seemed like largely a waste of time. Possibly, the position already had an accepted candidate.
+
+Interview Questions
+
+Code the boogle algorithm.
+
+
+   // https://www.geeksforgeeks.org/boggle-find-possible-words-board-characters/
+
+   Given a dictionary, a method to do lookup in dictionary and a M x N board where every cell has one character.
+   Find all possible words that can be formed by a sequence of adjacent characters. Note that we can move to any
+   of 8 adjacent characters, but a word should not have multiple instances of same cell.
+
+  Input: dictionary[] = {"GEEKS", "FOR", "QUIZ", "GO"};
+  boggle[][]   = {{'G','I','Z'},
+                  {'U','E','K'},
+                  {'Q','S','E'}};
+  isWord(str): returns true if str is present in dictionary else false.
+
+  Output:  Following words of dictionary are present
+            GEEKS
+            QUIZ
+*/
+
+#endif //PNSEI_BOGGLE_H
