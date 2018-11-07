0caf4729a89f054a81786b8daedf4d4df34f7a5c
shadowwalker2718 <shadowwalker2718@gmail.com>
2018-11-06 23:53:48 -0500

    cruise

--- cpp/cruise/distributed_LRU.h
+++ cpp/cruise/distributed_LRU.h
@@ -30,6 +30,21 @@ function. 打开coderpad要用python写，问可不可以java，同意后就用j
 请问LRU， follow up templated version 是什么意思
 
 就是存的node value不一定是int,是T怎么写，需要自定义hash function。
+ *
+ *
+ * 2018(7-9月) 码农类General 硕士 全职@Cruise - 猎头 - Onsite  | Other | 在职跳槽
+1点开始4点半结束，4个面试官，连饭好不好吃都不给体验一下的。。。productivitiy 组. 一亩-三分-地，独家发布
+1。聊了挺久现在在做啥，然后上个design问题，因为面试官的是productivity组里的搭测试framework的，所以就问我要怎么样搭个给他们跑测试的环境
+ ，刚好和我现在工作的framework沾边，所以可以扯挺多。他们每个test都需要跑很长时间，而且test data也很大，要怎样优化这些。
+ 讲一些scalability的东东，跟面试官聊的挺不错的感觉
+2。manager聊天，了解这个组的情况，没什么behavior问题就问问你做什么，对cruise想法怎样.
+3。白板，基本上可以算是golang问题。。。如果你有好几个ordered stream（channel），如何合成一个ordered stream，写完后面试官说我的答案
+ 没用最优的golang pattern，中间还得问面试官golang的channel syntax，嘤嘤嘤，如果我挂了肯定是因为这个！.
+4.  两人坐下来面对面写coderpad。。。不用自己带电脑，他们提供个chromebook。题目是LRU cache，笔者刷题不精，没刷过这个经典题，不过算是从
+ 头导出了Queue + hashmap的解法。。。好处就是不用装没刷过（emmm）
+
+今早recruiter发邮件说明天约电话时间，看了地里面筋，如果拒就是模版据信，所以应该是有戏？地点在SF的costco旁边，离地铁有点远就是了。
+ *
  * */
 
 namespace cruise_20181009 {
