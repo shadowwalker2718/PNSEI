bea7b7675479d92acb669e0c920a6eecefc1025e
Henry Fuheng Wu <wufuheng@gmail.com>
2018-11-08 22:16:47 -0500

    skiplist insertnode

--- cpp/skiplist.h
+++ cpp/skiplist.h
@@ -2,6 +2,9 @@
 #include "henry.h"
 #include <random>
 
+
+// very good!!!
+// http://ticki.github.io/blog/skip-lists-done-right/
 namespace _skiplist {
 #define MaxLevel 5
 
@@ -9,7 +12,7 @@ typedef struct SkipListNode {
   int key;
   int value;
   int level;
-  SkipListNode *forward[MaxLevel];
+  SkipListNode *next[MaxLevel];
 } SkipListNode;
 
 typedef struct SkipList {
@@ -23,8 +26,8 @@ SkipListNode *makeNode(int level, int key, int value) {
   pNode->key = key;
   pNode->value = value;
   pNode->level = level;
-  for (int i = 0; i < MaxLevel; ++i)
-    pNode->forward[i] = NULL;
+  for (int i = 0; i < MaxLevel; ++i) // every node has MaxLevel children, some of them are NULL
+    pNode->next[i] = NULL;
   return pNode;
 }
 
@@ -32,21 +35,15 @@ void initSkipList(SkipList *pSkipList) {
   if (!pSkipList)
     return;
   srand(0xdeadbeef);
-  SkipListNode *head = makeNode(0, 0, 0);
-  if (!head)
-    return;
-  pSkipList->head = head;
+  pSkipList->head = makeNode(-1, -1, -1);
   pSkipList->length = 0;
   pSkipList->level = 1;
-
-  for (int i = 0; i < MaxLevel; i++)
-    pSkipList->head->forward[i] = NULL;
 }
 
 int randomLevel() {
   // return rand() % MaxLevel;
   int newLevel = 1;
-  while ((rand() & 0xFFFF) < (0.4 * 0xFFFF))
+  while (rand() < 0.4 * RAND_MAX)
     ++newLevel;
   return min(newLevel, MaxLevel);
 }
@@ -55,29 +52,53 @@ bool insertNode(SkipList *pSkipList, int searchKey, int newValue) {
   if (!pSkipList)
     return false;
 
-  SkipListNode *update[MaxLevel]={};
+  SkipListNode *update[MaxLevel]={};////
   //FindGreaterOrEqual
-  SkipListNode *cur = pSkipList->head;
+  SkipListNode *cur = pSkipList->head; // insert position
+
+  /*int k=cur->level-1;
+  while (k>=0){
+    tn=cur->next[k]; //temp biggest child node
+    if(tn){
+      int ck=tn->key;
+      if(ck < searchKey){
+        k--;
+      }else if(ck > searchKey){
+        cur=tn;
+      }else{
+        tn->value=newValue;
+        return true;
+      }
+    }else{
+      k--;
+    }
+  }
+  //insert after cur
+  int h = randomLevel();
+  // cut off and splice the lines
+  auto nn=makeNode(h,searchKey, newValue);*/
+
   // from top to bottom
   for (int i = pSkipList->level - 1; i >= 0; i--) {
-    while (cur->forward[i] && cur->forward[i]->key < searchKey)
-      cur = cur->forward[i];
+    while (cur->next[i] && cur->next[i]->key < searchKey)
+      cur = cur->next[i];
     update[i] = cur;
   }
-  cur = cur->forward[0];
+  cur = cur->next[0];
   if (cur && cur->key == searchKey) {
     cur->value = newValue;
   } else {
     int k = randomLevel();
     if (k > pSkipList->level) {
       for (int i = pSkipList->level; i < k; i++)
-        update[i] = pSkipList->head;
+        update[i] = pSkipList->head; ////////
       pSkipList->level = k;
     }
-    cur = makeNode(k, searchKey, newValue);
+    auto nn = makeNode(k, searchKey, newValue);
     for (int i = 0; i < k; ++i) {
-      cur->forward[i] = update[i]->forward[i];
-      update[i]->forward[i] = cur;
+      auto tmp=update[i]->next[i]; // insert a new node in the linked list
+      update[i]->next[i] = nn;
+      nn->next[i] = tmp;
     }
     pSkipList->length++;
   }
@@ -93,11 +114,11 @@ SkipListNode *searchNode(SkipList *pSkipList, int searchKey) {
     return NULL;
 
   for (int i = pSkipList->level - 1; i >= 0; --i) {
-    while (pNode->forward[i] && pNode->forward[i]->key < searchKey)
-      pNode = pNode->forward[i];
+    while (pNode->next[i] && pNode->next[i]->key < searchKey)
+      pNode = pNode->next[i];
   }
 
-  pNode = pNode->forward[0];
+  pNode = pNode->next[0];
   if (pNode && pNode->key == searchKey)
     return pNode;
   return NULL;
@@ -111,23 +132,23 @@ bool deleteNode(SkipList *pSkipList, int searchKey) {
   SkipListNode *update[MaxLevel];
 
   for (int i = pSkipList->level - 1; i >= 0; i--) {
-    while (pNode->forward[i] && pNode->forward[i]->key < searchKey) {
-      pNode = pNode->forward[i];
+    while (pNode->next[i] && pNode->next[i]->key < searchKey) {
+      pNode = pNode->next[i];
     }
     update[i] = pNode;
   }
 
-  pNode = pNode->forward[0];
+  pNode = pNode->next[0];
   if (pNode && pNode->key == searchKey) {
     for (int i = 0; i < pSkipList->level; ++i) {
-      if (update[i] && update[i]->forward[i] != pNode) {
+      if (update[i] && update[i]->next[i] != pNode) {
         break;
       }
-      update[i]->forward[i] = pNode->forward[i];
+      update[i]->next[i] = pNode->next[i];
     }
     free(pNode);
     while (pSkipList->level > 1 &&
-           pSkipList->head->forward[pSkipList->level - 1] == NULL) {
+           pSkipList->head->next[pSkipList->level - 1] == NULL) {
       pSkipList->level--;
     }
     pSkipList->length--;
@@ -141,9 +162,9 @@ void travelList(SkipList *pSkipList) {
   if (!pNode)
     return;
 
-  while (pNode->forward[0]) {
-    cout << pNode->forward[0]->value << " " << pNode->forward[0]->level << endl;
-    pNode = pNode->forward[0];
+  while (pNode->next[0]) {
+    cout << pNode->next[0]->value << " " << pNode->next[0]->level << endl;
+    pNode = pNode->next[0];
   }
 }
 
@@ -156,7 +177,21 @@ int test() {
   insertNode(&sl, 7,7);
   insertNode(&sl, 3,3);
   insertNode(&sl, 37,37);
-  insertNode(&sl, 37,73);
+  insertNode(&sl, 11,11);
+  insertNode(&sl, 55,55);
+  insertNode(&sl, 70,70);
+  insertNode(&sl, 86,86);
+  insertNode(&sl, 98,98);
+  insertNode(&sl, 108,108);
+  insertNode(&sl, 87,87);
+  insertNode(&sl, 93,93);
+  insertNode(&sl, 137,137);
+  insertNode(&sl, 111,111);
+  insertNode(&sl, 155,155);
+  insertNode(&sl, 170,170);
+  insertNode(&sl, 186,186);
+  insertNode(&sl, 198,198);
+  insertNode(&sl, 208,208);
   travelList(&sl);
 
   SkipList list;
