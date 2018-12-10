9d47956dc065f318556afbd91176606bfa619e98
Henry Fuheng Wu <wufuheng@gmail.com>
2018-12-10 18:03:37 -0500

    cruise

---
+++ cpp/cruise/christmas_tree.h
@@ -0,0 +1,66 @@
+//
+// Created by Henry Wu on 12/10/18.
+//
+
+#ifndef PNSEI_CHRISTMAS_TREE_H
+#define PNSEI_CHRISTMAS_TREE_H
+
+/*
+2018(10-12月) 码农类General 硕士 全职@Cruise - 内推 - Onsite  | Pass | 在职跳槽
+第一次发帖，因为实在是没大米看帖子，只读模式太久了, 想想还是要为地里做点贡献：
+
+店面：生成一颗圣诞树, 提供一个argument K 代表高度。例子：
+
+K = 4
+  #
+ ###
+#####
+  #
+
+
+K = 5
+   #
+  ###
+ #####
+#######
+   #
+
+
+follow up 1:
+圣诞树顶端变*。例子：
+
+K = 4
+  *
+ ###
+#####
+  #
+
+K = 5
+   *
+  ###
+ #####
+#######
+   #
+
+follow up 2: 中空圣诞树, 但是倒数第二行不变 。例子：
+K = 4
+  *
+ # #
+#####
+  #
+
+K = 5
+   *
+  # #
+ #   #
+#######
+   #
+店面过了， 但是这个组没hc了，换了个组，又要重新店面。
+利口散林妖
+*/
+
+namespace _cruise_christmastree{
+
+}
+
+#endif //PNSEI_CHRISTMAS_TREE_H
